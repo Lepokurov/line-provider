b8f9d875ebf64320d8358fc332943ec16d86a47f
Andrey Lepokurov <endreyva@gmail.com>
2022-07-30 22:09:24 +0700

    include models to main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+from api import ping_router, event_router
+from events.error import controller_exceptions_handler, BaseEventControllerException
+
+app = FastAPI(
+    title="line-provider",
+    version="1.0.0",
+    description="Сервис line-provide",
+)
+
+app.include_router(ping_router)
+app.include_router(event_router)
+app.add_exception_handler(BaseEventControllerException, controller_exceptions_handler)
